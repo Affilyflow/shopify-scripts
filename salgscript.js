f10a488c0be84213bf435aa0b3d5685661583d1c
Affilyflow <167556081+Affilyflow@users.noreply.github.com>
2024-05-17 20:46:09 +0200

    Update salgscript.js

--- salgscript.js
+++ salgscript.js
@@ -1,52 +1,58 @@
-  document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     console.log("JavaScript Loaded");
 
     function getCookie(name) {
-      var value = "; " + document.cookie;
-      var parts = value.split("; " + name + "=");
-      if (parts.length === 2) return parts.pop().split(";").shift();
+        var value = "; " + document.cookie;
+        var parts = value.split("; " + name + "=");
+        if (parts.length === 2) return parts.pop().split(";").shift();
     }
 
     if (window.Shopify && Shopify.checkout) {
-      console.log("Shopify checkout object found");
-      
-      var orderId = Shopify.checkout.order_id;
-      var aff_id = getCookie('affiliate_id');
-      var network = getCookie('network');
-      var store = getCookie('store');  // Retrieve the store cookie
-      var fullUrl = getCookie('full_url'); // Retrieve the full URL cookie
-      var orderTotal = Shopify.checkout.total_price;
-      var currency = Shopify.checkout.currency;
-      var productNames = Shopify.checkout.line_items.map(item => item.title);
+        console.log("Shopify checkout object found");
+        
+        var orderId = Shopify.checkout.order_id;
+        var aff_id = getCookie('affiliate_id');
+        var network = getCookie('network');
+        var store = getCookie('store');  // Retrieve the store cookie
+        var fullUrl = getCookie('full_url'); // Retrieve the full URL cookie
+        var orderTotal = Shopify.checkout.total_price;
+        var currency = Shopify.checkout.currency;
+        
+        // Create a map of product names to product IDs
+        var productMap = Shopify.checkout.line_items.reduce((map, item) => {
+            map[item.title] = item.variant_id;
+            return map;
+        }, {});
 
-      console.log("Order ID: " + orderId);
-      console.log("Affiliate ID: " + aff_id);
-      console.log("Network: " + network);
-      console.log("Store: " + store);
-      console.log("Full URL: " + fullUrl);
-      console.log("Order Total: " + orderTotal);
-      console.log("Currency: " + currency);
-      console.log("Product Names: " + productNames.join(", "));
+        console.log("Order ID: " + orderId);
+        console.log("Affiliate ID: " + aff_id);
+        console.log("Network: " + network);
+        console.log("Store: " + store);
+        console.log("Full URL: " + fullUrl);
+        console.log("Order Total: " + orderTotal);
+        console.log("Currency: " + currency);
+        console.log("Product Map: ", productMap);
 
-      var xhr = new XMLHttpRequest();
-      xhr.open("POST", "https://xepn-38qp-in4n.f2.xano.io/api:-WVr0FO_/sales/salg", true);
-      xhr.setRequestHeader("Content-Type", "application/json");
-      console.log("Preparing to send AJAX request");
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://xepn-38qp-in4n.f2.xano.io/api:-WVr0FO_/sales/salg", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        console.log("Preparing to send AJAX request");
 
-      xhr.send(JSON.stringify({
-        aff_id: aff_id,
-        network: network,
-        order_id: orderId,
-        store: store,
-        full_url: fullUrl,
-        order_total: orderTotal,
-        currency: currency,
-        product_names: productNames
-      }));
+        xhr.send(JSON.stringify({
+            aff_id: aff_id,
+            network: network,
+            order_id: orderId,
+            store: store,
+            full_url: fullUrl,
+            order_total: orderTotal,
+            currency: currency,
+            product_names: productMap
+        }));
 
-      console.log("AJAX request sent");
+        console.log("AJAX request sent");
     }
-  });
+});
+
 function sendHeartbeat() {
     var storeUrl = window.location.origin; // Get the full base URL, including the scheme
     var xhr = new XMLHttpRequest();
@@ -66,4 +72,3 @@ document.addEventListener("DOMContentLoaded", function() {
         sendHeartbeat();
     }
 });
-
