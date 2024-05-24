f8a8f785e9bacf96dc2640cfb3145641c9c3391f
Affilyflow <167556081+Affilyflow@users.noreply.github.com>
2024-05-24 16:21:08 +0200

    Update shopclickcookiescript.js

--- shopclickcookiescript.js
+++ shopclickcookiescript.js
@@ -8,7 +8,6 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     const cookieString = `${name}=${value || ""}${expires}; path=/;`;
     document.cookie = cookieString;
-    console.log(`Cookie set: ${cookieString}`);
   }
 
   function getCookie(name) {
@@ -23,27 +22,18 @@ document.addEventListener("DOMContentLoaded", function() {
   const store = urlParams.get('store'); // Get 'store' parameter from the URL
   const referrer = document.referrer || "Direct"; // Get the referrer or set to "Direct" if no referrer is available
 
-  console.log('Affiliate ID:', affiliateId);
-  console.log('Network:', network);
-  console.log('Store:', store);
-  console.log('Referrer:', referrer);
-
   if (affiliateId && network && store) {
     setCookie('affiliate_id', affiliateId, 40);
     setCookie('network', network, 40);
     setCookie('store', store, 40);
     setCookie('full_url', window.location.href, 40);
     setCookie('referrer', referrer, 40); // Set the referrer as a cookie
-  } else {
-    console.log('Missing one or more parameters: affiliateId, network, store');
   }
 
   // Check if referrer cookie is set, if not set it
   if (!getCookie('referrer')) {
     setCookie('referrer', referrer, 40);
   }
-
-  console.log('Current Cookies:', document.cookie);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -61,13 +51,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var full_url = window.location.href; // Get the full URL of the current page
   var referrer = document.referrer || "Direct"; // Get the referrer or set to "Direct"
 
-  console.log('Query Parameters:');
-  console.log('aff_id:', aff_id);
-  console.log('network:', network);
-  console.log('store:', store);
-  console.log('full_url:', full_url);
-  console.log('referrer:', referrer);
-
   // Send the data to the API if the network is 'affilyflow'
   if (network && (network.toLowerCase() === 'affilyflow')) {
     fetch('https://xepn-38qp-in4n.f2.xano.io/api:-WVr0FO_/clicks', {
@@ -80,18 +63,14 @@ document.addEventListener('DOMContentLoaded', function() {
         network: network, 
         store: store,
         full_url: full_url, // Include the full URL in the payload
-        refferer: refferer,  // Include the referrer in the payload
+        referrer: referrer // Include the referrer in the payload
       })
     })
     .then(response => response.json())
-    .then(data => {
-      console.log('API Response:', data);
-    })
     .catch(error => {
       console.error('API Error:', error);
     });
-  } else {
-    console.log('Network is not affilyflow');
   }
 });
+
 
